8d7617fddbe3754c7540c9508148f3752ca0b4ba
zotovan <zotov.andrey@gmail.com>
2013-08-19 15:08:29 +0400

    Fix 2

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-var http = require('http');
+//var http = require('http');
 
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
-    
-}).listen(process.env.PORT || 8080);
+//http.createServer(function (req, res) {
+//    
+//    res.writeHead(200, { 'Content-Type': 'text/html' });
+//    res.end('Hello, world!');
+//    
+//}).listen(process.env.PORT || 8080);
+
+  var express = require('express');
+  var http = require('http');
+  var index = require('./routes/index.js');
+
+  var app = express();
+          
+   
+  app.get('/', index.index);
+
+  http.createServer(app).listen(process.env.PORT || 80, function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
